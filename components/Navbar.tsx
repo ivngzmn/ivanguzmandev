0d438b84fbd80957e88b3aede3e19ed67331c611
Ivan Guzman <n3cr0.2012@gmail.com>
2021-11-05 01:17:24 -0700

    updated mobile menu label name 🏷

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -183,7 +183,7 @@ export default function Navbar() {
               </NextLink>
               <NextLink href="/contact">
                 <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-                  Projects
+                  Contact
                 </a>
               </NextLink>
 
