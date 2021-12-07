25fa2e7afa7cfb8e673e227f890a3f3ec290f475
Ivan Guzman <n3cr0.2012@gmail.com>
2021-12-07 08:46:47 -0800

    WIP: comment out for now 🤷🏽‍♂️

--- pages/api/userconfig.tsx
+++ pages/api/userconfig.tsx
@@ -1,12 +1,12 @@
-import Avatar from '../assets/avatar.jpg';
+// import Avatar from '../public/images/avatar.jpg';
 
-export const userConfig = {
-  avatar: Avatar,
-  firstName: 'Ivan',
-  lastName: 'Guzman',
-  username: 'zaku_dev',
-  about: `I'm a Full Stack developer and freelancer that specializes in making the web faster and more accessible for small businesses`,
-  phoneNumber: '714-773-2050',
-  country: 'United States',
-  state: 'California'
-};
+// export const userConfig = {
+//   avatar: Avatar,
+//   firstName: 'Ivan',
+//   lastName: 'Guzman',
+//   username: 'zaku_dev',
+//   about: `I'm a Full Stack developer and freelancer that specializes in making the web faster and more accessible for small businesses`,
+//   phoneNumber: '714-773-2050',
+//   country: 'United States',
+//   state: 'California'
+// };
