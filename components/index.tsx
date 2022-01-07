c1203b75fe92ed5dccac769fcf3bce6141c6a0ce
Ivan Guzman <n3cr0.2012@gmail.com>
2022-01-07 00:59:39 -0800

    Updated Links page

--- components/index.tsx
+++ components/index.tsx
@@ -19,3 +19,4 @@ export { default as Comments } from './Comments';
 export { default as CommentsForm } from './CommentsForm';
 export { default as Loader } from './Loader';
 export { default as Setup } from './Setup';
+export { default as MyLinks } from './MyLinks';
