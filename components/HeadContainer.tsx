4fb801bda0da0d5ab7320156bbf5cd308e898239
Ivan Guzman <n3cr0.2012@gmail.com>
2022-01-19 03:41:36 -0800

    Updated meta tag

--- components/HeadContainer.tsx
+++ components/HeadContainer.tsx
@@ -32,7 +32,7 @@ export default function Container(props) {
         href={`https://www.ivanguzman.dev/${router.asPath}`}
       />
       <meta property="og:type" content={meta.type} />
-      <meta property="og:site_name" content="Lee Robinson" />
+      <meta property="og:site_name" content="Ivan Guzman" />
       <meta property="og:description" content={meta.description} />
       <meta property="og:title" content={meta.title} />
       <meta property="og:image" content={meta.image} />
