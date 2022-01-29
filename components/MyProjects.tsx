823a5141313bcb1a32de7fd09d58044ff9b15785
Ivan Guzman <n3cr0.2012@gmail.com>
2022-01-28 21:38:42 -0800

    Updated coinforge project link

--- components/MyProjects.tsx
+++ components/MyProjects.tsx
@@ -9,7 +9,7 @@ import { ProjectCard } from 'components';
 const projects = [
   {
     name: 'Coin Forge',
-    liveLink: 'https://coinforge.netlify.app/',
+    liveLink: 'https://coinforge.vercel.app/',
     githubLink: 'https://github.com/ivngzmn/coin-forge',
     imageSrc: CoinForge,
     imageAlt: 'Coin Forge',
@@ -29,17 +29,17 @@ const projects = [
 export default function myProjects() {
   return (
     <div className="bg-zinc-900">
-      <div className="relative max-w-7xl mx-auto pb-20 px-4 sm:px-6 lg:px-8">
+      <div className="relative mx-auto max-w-7xl px-4 pb-20 sm:px-6 lg:px-8">
         <div className="mb-2 lg:mb-8">
-          <div className="w-full inline-block py-10 lg:py-20">
-            <div className="md:float-left block">
+          <div className="inline-block w-full py-10 lg:py-20">
+            <div className="block md:float-left">
               <span
-                className="text-6xl lg:text-8xl font-semibold leading-8 tracking-tight"
+                className="text-6xl font-semibold leading-8 tracking-tight lg:text-8xl"
                 id="projects"
               >
                 Projects
               </span>
-              <h2 className="text-3xl lg:text-4xl font-light tracking-normal text-slate-50 mt-5">
+              <h2 className="mt-5 text-3xl font-light tracking-normal text-slate-50 lg:text-4xl">
                 Selected works and client projects.
               </h2>
             </div>
@@ -48,19 +48,19 @@ export default function myProjects() {
 
         {/* projects section */}
 
-        <div className="pt-2 space-y-6 lg:space-y-40 relative">
+        <div className="relative space-y-6 pt-2 lg:space-y-40">
           {/* <ProjectCard /> */}
           {projects.map((project) => (
             <div
               key={project.name}
-              className=" pb-10 bg-transparent lg:pb-0 lg:z-10 lg:relative rounded-xl"
+              className=" rounded-xl bg-transparent pb-10 lg:relative lg:z-10 lg:pb-0"
             >
-              <div className="lg:mx-auto lg:max-w-full lg:grid lg:grid-cols-12">
-                <div className="relative z-10 lg:col-start-1 lg:row-start-1 lg:col-span-8 lg:py-0">
+              <div className="lg:mx-auto lg:grid lg:max-w-full lg:grid-cols-12">
+                <div className="relative z-10 lg:col-span-8 lg:col-start-1 lg:row-start-1 lg:py-0">
                   <div className="mx-auto max-w-3xl px-4 pt-4 sm:max-w-3xl sm:px-6 lg:p-0">
-                    <div className="w-full h-[30rem] sm:w-full lg:h-[800px] duration-500 ease-in-out">
+                    <div className="h-[30rem] w-full duration-500 ease-in-out sm:w-full lg:h-[800px]">
                       <Image
-                        className="w-full h-full object-center rounded-xl"
+                        className="h-full w-full rounded-xl object-center"
                         src={project.imageSrc}
                         alt={project.imageAlt}
                         objectFit="cover"
@@ -69,28 +69,28 @@ export default function myProjects() {
                     </div>
                   </div>
                 </div>
-                <div className="mt-12 lg:m-0 lg:px-4 lg:col-span-4 lg:pl-8">
-                  <div className="mx-auto max-w-md px-6 sm:max-w-2xl sm:px-6 lg:px-0 lg:py-20 lg:max-w-none">
+                <div className="mt-12 lg:col-span-4 lg:m-0 lg:px-4 lg:pl-8">
+                  <div className="mx-auto max-w-md px-6 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0 lg:py-20">
                     <div>
-                      <span className="text-white text-4xl lg:text-5xl font-regular">
+                      <span className="font-regular text-4xl text-white lg:text-5xl">
                         {project.name}
                       </span>
-                      <p className="mt-6 text-lg lg:text-xl font-regular text-white">
+                      <p className="font-regular mt-6 text-lg text-white lg:text-xl">
                         {project.description}
                       </p>
                     </div>
                     <div className="mt-8 lg:w-2/3">
                       <Link href={project.liveLink}>
-                        <a className="transition duration-500 ease-in-out transform hover:-translate-y-1 w-full space-x-3 flex items-center justify-center px-8 py-3 border-transparent text-base font-regular lg:font-semibold rounded-md text-white bg-gradient-to-r from-purple-600 to-indigo-600 hover:from-purple-700 hover:to-indigo-700 md:py-4 md:text-xl md:px-10">
-                          <FaRegPaperPlane className="w-6 h-6" />
+                        <a className="font-regular flex w-full transform items-center justify-center space-x-3 rounded-md border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 px-8 py-3 text-base text-white transition duration-500 ease-in-out hover:-translate-y-1 hover:from-purple-700 hover:to-indigo-700 md:py-4 md:px-10 md:text-xl lg:font-semibold">
+                          <FaRegPaperPlane className="h-6 w-6" />
                           <span>Live</span>
                         </a>
                       </Link>
                     </div>
                     <div className="mt-8 lg:w-2/3">
                       <Link href={project.githubLink}>
-                        <a className="transition duration-500 ease-in-out transform hover:-translate-y-1 w-full space-x-3 flex items-center justify-center px-8 py-3 border border-transparent text-base font-regular lg:font-semibold rounded-md text-indigo-700 bg-white hover:bg-indigo-100 md:py-4 md:text-xl md:px-10">
-                          <FaGithub className="w-6 h-6" /> <span>Source</span>
+                        <a className="font-regular flex w-full transform items-center justify-center space-x-3 rounded-md border border-transparent bg-white px-8 py-3 text-base text-indigo-700 transition duration-500 ease-in-out hover:-translate-y-1 hover:bg-indigo-100 md:py-4 md:px-10 md:text-xl lg:font-semibold">
+                          <FaGithub className="h-6 w-6" /> <span>Source</span>
                         </a>
                       </Link>
                     </div>
