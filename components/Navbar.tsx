117c960b85025e048a66fa68f7b751ce07afb2c9
Ivan Guzman <hello@ivanguzman.dev>
2022-04-06 19:23:54 -0700

    Fix: link to resume

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -160,7 +160,7 @@ export default function Navbar() {
                           <Menu.Item>
                             {({ active }) => (
                               <a
-                                href="https://www.linkedin.com/in/ivan-julian-guzman/"
+                                href="/resume"
                                 className={classNames(
                                   active ? 'bg-gray-700' : '',
                                   'mt-5 flex items-center px-4 py-2 text-sm text-gray-300'
