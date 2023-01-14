1df3b3eba4aa17fca36ef28ee5ed124ca4143f76
Ivan Guzman <ivanguzmandev@gmail.com>
2023-01-14 02:52:47 -0800

    Updated about page to include the work history

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,7 +2,10 @@ import Image from 'next/future/image'
 import Head from 'next/head'
 import Link from 'next/link'
 import clsx from 'clsx'
-
+import logo100Devs from '@/images/logos/100devs.png'
+import logoUber from '@/images/logos/uber.png'
+import logoZeek from '@/images/logos/zeek.png'
+import logoMarineDepot from '@/images/logos/marineDepot.jpeg'
 import { Container } from '@/components/Container'
 import {
   TwitterIcon,
@@ -11,6 +14,7 @@ import {
   LinkedInIcon,
 } from '@/components/SocialIcons'
 import portraitImage from '@/images/portrait.webp'
+import {Button} from "@/components/Button";
 
 function SocialLink({ className, href, children, icon: Icon }) {
   return (
@@ -36,6 +40,131 @@ function MailIcon(props) {
     </svg>
   )
 }
+function BriefcaseIcon(props) {
+  return (
+      <svg
+          viewBox="0 0 24 24"
+          fill="none"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          aria-hidden="true"
+          {...props}
+      >
+        <path
+            d="M2.75 9.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
+            className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
+        />
+        <path
+            d="M3 14.25h6.249c.484 0 .952-.002 1.316.319l.777.682a.996.996 0 0 0 1.316 0l.777-.682c.364-.32.832-.319 1.316-.319H21M8.75 6.5V4.75a2 2 0 0 1 2-2h2.5a2 2 0 0 1 2 2V6.5"
+            className="stroke-zinc-400 dark:stroke-zinc-500"
+        />
+      </svg>
+  )
+}
+function ArrowDownIcon(props) {
+  return (
+      <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
+        <path
+            d="M4.75 8.75 8 12.25m0 0 3.25-3.5M8 12.25v-8.5"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+      </svg>
+  )
+}
+function Resume() {
+  let resume = [
+    {
+      company: 'Zeek Interactive',
+      title: 'Software Engineer',
+      logo: logoZeek,
+      start: '2022',
+      end: {
+        label: 'Present',
+        dateTime: new Date().getFullYear(),
+      },
+    },
+    {
+      company: '100Devs',
+      title: 'Software Engineer',
+      logo: logo100Devs,
+      start: '2020',
+      end: '2022',
+    },
+    {
+      company: 'Uber',
+      title: 'Customer Service Specialist',
+      logo: logoUber,
+      start: '2019',
+      end: '2020',
+    },
+    {
+      company: 'Marine Depot',
+      title: 'Shift Supervisor',
+      logo: logoMarineDepot,
+      start: '2016',
+      end: '2019',
+    },
+  ]
+
+  return (
+      <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
+        <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+          <BriefcaseIcon className="h-6 w-6 flex-none" />
+          <span className="ml-3">Work</span>
+        </h2>
+        <ol className="mt-6 space-y-4">
+          {resume.map((role, roleIndex) => (
+              <li key={roleIndex} className="flex gap-4">
+                <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+                  <Image
+                      src={role.logo}
+                      alt=""
+                      className="h-7 w-7 rounded-full"
+                      unoptimized
+                  />
+                </div>
+                <dl className="flex flex-auto flex-wrap gap-x-2">
+                  <dt className="sr-only">Company</dt>
+                  <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
+                    {role.company}
+                  </dd>
+                  <dt className="sr-only">Role</dt>
+                  <dd className="text-xs text-zinc-500 dark:text-zinc-400">
+                    {role.title}
+                  </dd>
+                  <dt className="sr-only">Date</dt>
+                  <dd
+                      className="ml-auto text-xs text-zinc-400 dark:text-zinc-500"
+                      aria-label={`${role.start.label ?? role.start} until ${
+                          role.end.label ?? role.end
+                      }`}
+                  >
+                    <time dateTime={role.start.dateTime ?? role.start}>
+                      {role.start.label ?? role.start}
+                    </time>{' '}
+                    <span aria-hidden="true">—</span>{' '}
+                    <time dateTime={role.end.dateTime ?? role.end}>
+                      {role.end.label ?? role.end}
+                    </time>
+                  </dd>
+                </dl>
+              </li>
+          ))}
+        </ol>
+        <Button
+            href="https://www.linkedin.com/in/ivan-j-guzman/"
+            variant="secondary"
+            className="group mt-6 w-full"
+        >
+          View my LinkedIn
+          <ArrowDownIcon className="h-4 w-4 rotate-[-90deg] stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
+        </Button>
+      </div>
+  )
+}
 
 export default function About() {
   return (
@@ -50,7 +179,7 @@ export default function About() {
       <Container className="mt-16 sm:mt-32">
         <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
           <div className="lg:pl-20">
-            <div className="max-w-xs px-2.5 lg:max-w-none">
+            <div className="max-w-xs mx-auto px-2.5 lg:max-w-none">
               <Image
                 src={portraitImage}
                 alt=""
@@ -93,7 +222,10 @@ export default function About() {
               </p>
             </div>
           </div>
-          <div className="lg:pl-20">
+          <div className="space-y-10 lg:pl-16 xl:pl-24">
+            <Resume />
+          </div>
+          <div className="lg:pl-0">
             <ul role="list">
               <SocialLink href="https://twitter.com/inadequate_Dev" icon={TwitterIcon}>
                 Follow on Twitter
