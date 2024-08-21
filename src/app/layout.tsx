a9a1245cbffcab5784009b40fd402bc9198b3ff8
Ivan Guzman <ivanguzmandev@gmail.com>
2024-08-20 19:34:34 -0700

    gtm only on production

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
-      <GoogleTagManager gtmId="GTM-NQGNPP7" />
+      {process.env.NODE_ENV === 'production' && (
+        <GoogleTagManager gtmId="GTM-NQGNPP7" />
+      )}
       <body className="flex h-full bg-zinc-50 dark:bg-black">
         <Providers>
           <div className="flex w-full">
