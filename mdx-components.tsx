60fdfbc3a5caca4c9fe141eed0609178225b7278
Ivan Guzman <ivanguzmandev@gmail.com>
2026-02-23 16:39:54 -0800

    cleanup unused import

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,4 @@
 import { type MDXComponents } from 'mdx/types'
-import { Children, useState } from 'react'
 import { Pre } from '@/components/Pre'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
